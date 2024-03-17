6535db3b5319a402673b6f075f15fb94f2fcc6fc
Hugo Barrera <exailer89@gmail.com>
2024-03-17 00:21:42 -0600

    Mostrando un Spinner de Carga

--- js/app.js
+++ js/app.js
@@ -51,6 +51,8 @@ function consultarApi(ciudad, pais) {
     const apiId = '974e9bd2a3b13e0be6a03fa77ede4ac4';
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${ciudad},${pais}&appid=${apiId}`;
 
+    Spinner(); // Muestra el Spinner de carga
+
     fetch(url)
         .then(respuesta => respuesta.json())
         .then(datos => {
@@ -106,4 +108,29 @@ function limpiarHTML() {
     while(resultado.firstChild) {
         resultado.removeChild(resultado.firstChild);
     }
+}
+
+function Spinner() {
+
+    limpiarHTML();
+
+    const divSpinner = document.createElement('div');
+    divSpinner.classList.add('sk-fading-circle');
+
+    divSpinner.innerHTML = `
+        <div class="sk-circle1 sk-circle"></div>
+        <div class="sk-circle2 sk-circle"></div>
+        <div class="sk-circle3 sk-circle"></div>
+        <div class="sk-circle4 sk-circle"></div>
+        <div class="sk-circle5 sk-circle"></div>
+        <div class="sk-circle6 sk-circle"></div>
+        <div class="sk-circle7 sk-circle"></div>
+        <div class="sk-circle8 sk-circle"></div>
+        <div class="sk-circle9 sk-circle"></div>
+        <div class="sk-circle10 sk-circle"></div>
+        <div class="sk-circle11 sk-circle"></div>
+        <div class="sk-circle12 sk-circle"></div>
+    `;
+
+    resultado.appendChild(divSpinner);
 }
